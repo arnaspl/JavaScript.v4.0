59c6984f0742bf8abec59c6964c74c67b1dbb5c9
arnaspl <60697454+arnaspl@users.noreply.github.com>
2021-11-09 15:03:12 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,73 @@
+// const isValid = false;
+
+// if (!isValid) {
+//     console.log('True')
+// } else {
+//     console.log('False')
+// }
+
+// const age = 10;
+// if (age) {
+//     console.log('True')
+// } else {
+//     console.log('False')
+// }
+
+// console.log(isValid);
+// console.log(age);
+
+// Task 2
+// const firstName = 'Edvardas';
+// const lastName = 'Salnis';
+
+// if (firstName === 'Edvardas' && (age === 1 || lastName === 'Salnis')) {
+//         console.log('True')
+// } else {
+//     console.log('False')
+// }
+
+// Task 3
+// const allowToBuy = age >= 20 ? 'Galima':'Negalima'
+// console.log(allowToBuy);
+
+// Task 4
+// function greet (personName) {
+//     const name = personName ? personName : 'Stranger';
+//     console.log(`Sveikas, ${name}`)
+// }
+
+
+// greet('Edvardas');
+
+// Task 5.1
+// const age = '15';
+// if (typeof age != 'number') {
+//     console.log('Netinkamas formatas')
+// } else if (age >= 20) {
+//     console.log ('Galite nusipirkti gerima')
+// } else if (age < 20) {
+//     console.log ('Negalite nusipirkti gerima ')
+// } else console.log('Bandykite dar karta')
+
+// Task 5.2
+// const person = {
+//     name: 'Edvardas',
+//     age: 19,
+//     isLithuanian: false
+// }
+
+// let citizen = 'negali balsuoti'
+
+// if (person.age >= 18 && person.isLithuanian) {
+//     citizen = 'gali balsuoti'
+// }
+
+// citizen = person.age >= 18 && person.isLithuanian ? 'Gali balsuoti' : 'Negali balsuoti'
+
+// console.log(citizen);
+
+// Jeigu norim, kad printintu tik tuomet, kai citizen nera tuscias
+// let citizen
+// if (citizen) console.log(citizen);
+
+const allow = (person.age >= 18 && person.isLithuanian) ? citizen = 'gali balsuoti' : 
